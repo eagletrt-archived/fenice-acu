6f9cf27be9c04cba3ebec431be1761b2016b3cc8
lucacaronti <carontiluca@gmail.com>
2019-10-03 18:48:01 +0200

    start id code

---
+++ ACU/Inc/id.h
@@ -0,0 +1,15 @@
+#ifndef ID_H
+#define ID_H
+
+typedef struct ID{
+	uint16_t imu_angular_rate;
+	uint16_t imu_acceleration;
+	uint16_t
+
+	//builder
+	ID();
+};
+
+
+
+#endif
---
+++ ACU/Src/id.c
@@ -0,0 +1,7 @@
+#include "id.h"
+
+ID(){
+	imu_angular_rate = 0x4EC;
+	imu_acceleration = 0x4ED;
+
+}
