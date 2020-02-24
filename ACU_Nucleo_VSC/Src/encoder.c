a910b6671620c045f314aa26e791b38dae7095db
lucacaronti <carontiluca@gmail.com>
2020-02-25 00:11:14 +0100

    moved encoder code to callback

--- ACU_Nucleo_VSC/Src/encoder.c
+++ ACU_Nucleo_VSC/Src/encoder.c
@@ -1,2 +1,18 @@
 #include "encoder.h"
 #include "global_variables.h"
+
+int polA_cont_up = 0;
+int polA_cont_down = 0;
+int polB_cont_down = 0;
+int polB_cont_up = 0;
+int cp;
+double resolution = 0.000005;
+double mult_fact = 5.238726792;
+double mult_fact2 = 5.170416667;
+double measurment_per = 0.4;
+double enc_speed = 0;
+double wheel_speed = 0;
+double wheel_speed2 = 0;
+
+int sig_a = RESET;
+int sig_b = RESET;
